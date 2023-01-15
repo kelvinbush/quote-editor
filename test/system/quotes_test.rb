162b9f57cfbdc686fbaa3f7f0f518b9b1b21bb7c
Kelvin Bush <kelybush@gmail.com>
2023-01-15 14:15:12 +0300

    generate quote model and add name validation
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
+  setup do
+    @quote = quotes(:first) # Reference to the first fixture quote
+  end
+
   test "Creating a new quote" do
     # When we visit the Quotes#index page
     # we expect to see a title with the text "Quotes"
@@ -21,5 +25,33 @@ class QuotesTest < ApplicationSystemTestCase
     # and to see our "Capybara quote" added to the list
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
+  end
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit quote"
+
+    fill_in "Name", with: "Updated quote"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
   end
 end

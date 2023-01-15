924e46a7e778ef647aebdcfcf1d36e63d21a5d33
Kelvin Bush <kelybush@gmail.com>
2023-01-15 15:19:13 +0300

    before turbo drive
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- db/migrate/20230115111344_create_quotes.rb
+++ db/migrate/20230115111344_create_quotes.rb
@@ -1,7 +1,7 @@
 class CreateQuotes < ActiveRecord::Migration[7.0]
   def change
     create_table :quotes do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end

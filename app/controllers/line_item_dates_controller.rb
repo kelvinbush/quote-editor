e480f45a9d51e205ff0e32ade9968be426f487d4
kelvin <kelybush@gmail.com>
2023-01-31 21:08:52 +0300

    add line item dates controller
    Signed-off-by: kelvin <kelybush@gmail.com>

---
+++ app/controllers/line_item_dates_controller.rb
@@ -0,0 +1,46 @@
+class LineItemDatesController < ApplicationController
+  before_action :set_quote
+  before_action :set_line_item_date, only: [:edit, :update, :destroy]
+
+  def edit
+  end
+
+  def destroy
+    @line_item_date.destroy
+
+    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+  end
+
+  def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @line_item_date = @quote.line_item_dates.build
+  end
+
+  def create
+    @line_item_date = @quote.line_item_dates.build(line_item_date_params)
+    if @line_item_date.save
+      redirect_to quote_path(@quote), notice: "Date was successfully created."
+    else render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:id])
+  end
+  def line_item_date_params
+    params.require(:line_item_date).permit(:date)
+  end
+
+  def set_quote
+    @quote = current_company.quotes.find(params[:quote_id])
+  end
+end

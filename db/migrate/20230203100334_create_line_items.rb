f8025e3cc2daf73cb1844173da35008596adf65c
kelvin <kelybush@gmail.com>
2023-02-03 13:05:24 +0300

    add constranints

--- db/migrate/20230203100334_create_line_items.rb
+++ db/migrate/20230203100334_create_line_items.rb
@@ -2,9 +2,9 @@ class CreateLineItems < ActiveRecord::Migration[7.0]
   def change
     create_table :line_items do |t|
       t.references :line_item_date, null: false, foreign_key: true
-      t.string :name
-      t.text :description
-      t.integer :quantity
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :quantity, null: false
       t.decimal :unit_price, precision: 10, scale: 2
 
       t.timestamps
